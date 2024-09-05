af8051a9324eec19c7ada44034e5240e10d61a7d
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-09-05 15:11:48 +0300

    create LoginView

---
+++ Messanger/App/MessangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessangerApp.swift
+//  Messanger
+//
+//  Created by Ilya Schevchenko on 05.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MessangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Messanger/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Messanger
+//
+//  Created by Ilya Schevchenko on 05.09.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
